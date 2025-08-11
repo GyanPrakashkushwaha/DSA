2f2705a5d8efbcb86125638ba11517ddc22316cf
GyanPrakashkushwaha <gyanp7880@gmail.com>
2025-08-11 07:17:01 +0530

    Learnt BFS and solved Count Provinces Problem!

---
+++ harshitaVerma/countProvinces.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.visited = None
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        return self.countProvinces(isConnected)
+    
+    def bfs(self, adjMatrix, start): # BFS Algorithm without a single change
+        queue = deque([start])
+        self.visited[start] = 1
+        while queue:
+            node = queue.popleft()
+            for neighbor in range(len(adjMatrix[node])):
+                if adjMatrix[node][neighbor] == 1 and node != neighbor and not self.visited[neighbor]:
+                    self.visited[neighbor] = 1
+                    queue.append(neighbor)
+            
+    def countProvinces(self, adjMatrix): # Very straight forward! Simply calling the BFS for all nodes for the disconnected graph.
+        self.visited = [0]*len(adjMatrix)
+        provinces = 0
+        for city in range(len(adjMatrix)):
+            if not self.visited[city]:
+                provinces += 1
+                self.bfs(adjMatrix, city)
+        
+        return provinces
