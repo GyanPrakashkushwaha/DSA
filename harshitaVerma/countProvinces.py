0a9ae87a3431cd8aeccb445622e084e7de10807f
GyanPrakashkushwaha <gyanp7880@gmail.com>
2025-08-11 07:38:23 +0530

    slight improvement

--- harshitaVerma/countProvinces.py
+++ harshitaVerma/countProvinces.py
@@ -26,3 +26,34 @@ class Solution:
                 self.bfs(adjMatrix, city)
         
         return provinces
+    
+    
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.visited = None
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        self.visited = [0]*len(isConnected)
+        provinces = 0
+        for city in range(len(isConnected)):
+            if not self.visited[city]:
+                provinces += 1
+                self.bfs(isConnected, city)
+        
+        return provinces
+
+    
+    def bfs(self, adjMatrix, start): 
+        queue = deque([start])
+        self.visited[start] = 1
+        while queue:
+            node = queue.popleft()
+            for neighbor in range(len(adjMatrix[node])):
+                if adjMatrix[node][neighbor] == 1 \
+                 and node != neighbor \
+                 and not self.visited[neighbor]:
+                    self.visited[neighbor] = 1
+                    queue.append(neighbor)
+            
